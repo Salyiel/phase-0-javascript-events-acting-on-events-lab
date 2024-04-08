f6008844985d67b362b805b99efbeb5f98293dee
Stacy <salyiela@icloud.com>
2024-04-08 15:33:34 +0300

    Complete lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+
+/* move dodger left */
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left -1}px`;
+    }
+}
+
+/* Event Listener Code */
+document.addEventListener("keydown", function(e) {
+    if(e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    if(e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+/* move dodger right */
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left +1}px`;
+    }
+}
+
